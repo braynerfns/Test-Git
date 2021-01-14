9b07955997c9d55d660886ddeabc6529d5326b8d
Rodrigo Brayner <g6rb32@gmail.com>
2021-01-14 17:16:59 -0300

    Projeto iniciado, mostrando a mensagem Voce Fuma Ganja?

---
+++ Teste Git/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Teste_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Você Fuma Ganja?");
+
+            Console.ReadLine();
+        }
+    }
+}
