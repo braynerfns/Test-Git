1a3437e1cbb2013ac523ea7acd3a438b934191ac
Rodrigo Brayner <g6rb32@gmail.com>
2021-01-14 18:18:13 -0300

    Vamos Fumar mais um

--- Teste Git/Program.cs
+++ Teste Git/Program.cs
@@ -9,6 +9,7 @@ namespace Teste_Git
             Console.WriteLine("Você Fuma Ganja?");
             Console.WriteLine("Prensado, Colombia e Skunk");
             Console.WriteLine("Não Fumo Prensado");
+            Console.WriteLine("Vamos Fumar mais um");
 
             Console.ReadLine();
         }
